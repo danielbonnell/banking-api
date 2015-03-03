7b046a20e5d8f78f8ff129a2753fe7d0313a46d4
Daniel Bonnell <daniel.bonnell@gmail.com>
2015-03-03 14:28:43 -0500

    Added username validation method skeleton

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :username,
+    presence: "Username can't be blank",
+    uniqueness: "Username already in use"
+  # validate :validate_username
+  #
+  # private
+  #
+  # def validate_username
+  #   true
+  # end
 end
