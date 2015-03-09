6c3ab5645326614b1e2956d15109825267fff5c4
Daniel Bonnell <daniel.bonnell@gmail.com>
2015-03-09 15:13:09 -0400

    Fixed bug causing rake tasks to hang indefinitely and then return an error saying that there are pending migrations

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# Set API end-point.
+BASE_URI = "https://#{ENV['TOKEN']}@sales.geezeo.com/api/v2"
+
 # Initialize the Rails application.
 Rails.application.initialize!
