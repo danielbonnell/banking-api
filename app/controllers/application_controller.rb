fe477680fdb7f6ad2b0a0a2d67be42ac9a53681a
Daniel Bonnell <daniel.bonnell@gmail.com>
2015-03-11 14:53:36 -0400

    Ping API before action an raise error if there is no response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :ping_api
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -14,5 +16,11 @@ class ApplicationController < ActionController::Base
       :password_confirmation,
       :current_password
     ]
+  end
+
+  def ping_api
+    unless JSON.parse(RestClient.get "#{BASE_URI}/ping").fetch("response") == "PONG"
+      raise ActionController::RoutingError.new('404: API could not be reached.')
+    end
   end
 end
